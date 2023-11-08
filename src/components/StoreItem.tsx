7a4b5943224543a0df23cc00551307aaf160b693
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-11-08 18:55:27 -0500

    created functions to increase, decrease, get, and remove quantity from cart

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import { Button, Card } from 'react-bootstrap'
 import { formatCurrency } from '../utils/formatCurrency'
+import { useShoppingCart } from '../context/ShoppingCartContext'
 
 type StoreItemProps = {
     id: number
@@ -9,7 +10,10 @@ type StoreItemProps = {
 }
 
 function StoreItem({id, name, price, imgUrl}: StoreItemProps ) {
-    const quantity = 1;
+    const {getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart} = useShoppingCart()
+
+    const quantity = getItemQuantity(id);
+
   return (
    <Card className='h-100'>
     <Card.Img variant='top' src={imgUrl} height="200px" style={{objectFit: "cover"}} />
@@ -20,16 +24,16 @@ function StoreItem({id, name, price, imgUrl}: StoreItemProps ) {
         </Card.Title> 
         <div className='mt-auto'>
             {quantity === 0 ? (
-                <Button className='w-100'>+ Add to Cart</Button>
+                <Button className='w-100' onClick={() => increaseCartQuantity(id)}>+ Add to Cart</Button>
             ) : <div className='d-flex align-items-center flex-column' style={{gap: ".5rem"}}>
                     <div className='d-flex align-items-center justify-content-center' style={{gap: ".5rem"}}>
-                        <Button>-</Button>
+                        <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
                         <div>
                             <span className='fs-3'>{quantity}</span> in cart
                         </div>
-                        <Button>+</Button>
+                        <Button onClick={() => increaseCartQuantity(id)}>+</Button>
                     </div>
-                    <Button variant='danger' size='sm'>Remove</Button>
+                    <Button variant='danger' size='sm' onClick={() => removeFromCart(id)}>Remove</Button>
                 </div>}
         </div>
     </Card.Body>
