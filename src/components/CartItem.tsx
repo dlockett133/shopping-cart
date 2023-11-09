9b8c183f05dee6db5bf8a6b245bb535cfabf500c
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-11-09 04:14:23 -0500

    updated img paths

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -11,13 +11,14 @@ function CartItem({id, quantity}: CartItemProps) {
 
     const {removeFromCart} = useShoppingCart()
     const item = storeItems.find(i => i.id === id)
+    const imageUrl = `.${item?.imgUrl}`
 
     if (!item) return null;
 
   return (
     <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
         <img 
-            src={item?.imgUrl} 
+            src={imageUrl} 
             alt={item?.name} 
             style={{width: "125px", height:"75px", objectFit: "cover"}}
         />
